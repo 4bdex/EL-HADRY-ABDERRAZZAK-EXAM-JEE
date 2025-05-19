070d04c89b42b96da07951062ea93b12b601ec26
Abdex <abdex4social@gmail.com>
2025-05-19 10:47:15 +0100

    add security

--- src/main/java/elhadry/abderrazzak/bank_backend/security/SecurityConfig.java
+++ src/main/java/elhadry/abderrazzak/bank_backend/security/SecurityConfig.java
@@ -1,105 +1,106 @@
 package elhadry.abderrazzak.bank_backend.security;
-// package ma.enset.app_backend.security;
 
-// import org.springframework.security.authentication.AuthenticationManager;
-// import org.springframework.security.authentication.ProviderManager;
-// import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-// import org.springframework.security.config.Customizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import javax.crypto.spec.SecretKeySpec;
 
-// import javax.crypto.spec.SecretKeySpec;
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    @Value("${jwt.secret}")
+    private String secretKey;
 
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.http.SessionCreationPolicy;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-// import org.springframework.security.oauth2.jwt.JwtDecoder;
-// import org.springframework.security.oauth2.jwt.JwtEncoder;
-// import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-// import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-// import org.springframework.security.web.SecurityFilterChain;
-// import org.springframework.web.cors.CorsConfiguration;
-// import org.springframework.web.cors.CorsConfigurationSource;
-// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails client = User.withUsername("client")
+                .password(passwordEncoder().encode("client"))
+                .roles("CLIENT")
+                .build();
+        UserDetails employe = User.withUsername("employe")
+                .password(passwordEncoder().encode("employe"))
+                .roles("EMPLOYE")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(client, employe, admin);
+    }
 
-// import com.nimbusds.jose.jwk.source.ImmutableSecret;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-// @Configuration
-// @EnableWebSecurity
-// @EnableMethodSecurity(prePostEnabled = true)
-// public class SecurityConfig {
-//     @Value("${jwt.secret}")
-//     private String secretKey;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/login", "/auth/profile").permitAll()
 
-//     @Bean
-//     public InMemoryUserDetailsManager userDetailsService() {
-//         UserDetails user = User.withUsername("user")
-//                 .password(passwordEncoder().encode("user"))
-//                 .roles("USER")
-//                 .build();
-//         UserDetails admin = User.withUsername("admin")
-//                 .password(passwordEncoder().encode("admin"))
-//                 .roles("ADMIN")
-//                 .build();
-//         return new InMemoryUserDetailsManager(user, admin);
-//     }
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
 
-//     @Bean
-//     public PasswordEncoder passwordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
+    @Bean
+    JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(secretKey.getBytes()));
+    }
 
-//     @Bean
-//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//         return http
-//                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                 .csrf(csrf -> csrf.disable())
-//                 .cors(Customizer.withDefaults())
-//                 .authorizeHttpRequests(auth -> auth
-//                         .requestMatchers("/auth/login", "/auth/profile").permitAll()
+    @Bean
+    JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "RSA");
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS256).build();
+    }
 
-//                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll())
-//                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-//                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-//                 .build();
-//     }
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) throws Exception {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(authProvider);
+    }
 
-//     @Bean
-//     JwtEncoder jwtEncoder() {
-//         return new NimbusJwtEncoder(new ImmutableSecret<>(secretKey.getBytes()));
-//     }
-
-//     @Bean
-//     JwtDecoder jwtDecoder() {
-//         SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "RSA");
-//         return NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS256).build();
-//     }
-
-//     @Bean
-//     public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) throws Exception {
-//         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-//         authProvider.setUserDetailsService(userDetailsService);
-//         authProvider.setPasswordEncoder(passwordEncoder());
-//         return new ProviderManager(authProvider);
-//     }
-
-//     @Bean
-//     CorsConfigurationSource corsConfigurationSource() {
-//         CorsConfiguration config = new CorsConfiguration();
-//         config.addAllowedOrigin("*");
-//         config.addAllowedHeader("*");
-//         config.addAllowedMethod("*");
-//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//         source.registerCorsConfiguration("/**", config);
-//         return source;
-//     }
-// }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
--- src/main/java/elhadry/abderrazzak/bank_backend/security/SecurityController.java
+++ src/main/java/elhadry/abderrazzak/bank_backend/security/SecurityController.java
@@ -1,65 +1,62 @@
 package elhadry.abderrazzak.bank_backend.security;
-// package ma.enset.app_backend.security;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 
-// import java.time.Instant;
-// import java.time.temporal.ChronoUnit;
-// import java.util.Map;
-// import java.util.stream.Collectors;
+@RestController
+@RequestMapping("/auth")
+public class SecurityController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.authentication.AuthenticationManager;
-// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-// import org.springframework.security.core.Authentication;
-// import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-// import org.springframework.security.oauth2.jwt.JwsHeader;
-// import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-// import org.springframework.security.oauth2.jwt.JwtEncoder;
-// import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+    @Autowired
+    private JwtEncoder jwtEncoder;
 
-// @RestController
-// @RequestMapping("/auth")
-// public class SecurityController {
-//     @Autowired
-//     private AuthenticationManager authenticationManager;
+    public SecurityController(AuthenticationManager authenticationManager, JwtEncoder jwtEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.jwtEncoder = jwtEncoder;
+    }
+    @GetMapping("/profile")
+    public Authentication authentication(Authentication auth) {
+        return auth;
+    }
 
-//     @Autowired
-//     private JwtEncoder jwtEncoder;
+    @PostMapping("/login")
+    public Map<String, String> login(String username, String password) {
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(username, password)
+        );
+        Instant instant = Instant.now();
+        String scope = auth.getAuthorities().stream()
+                .map(grantedAuthority -> grantedAuthority.getAuthority())
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
+                .subject(username)
+                .issuedAt(instant)
+                .expiresAt(instant.plus(15, ChronoUnit.MINUTES))
+                .claim("scope", scope)
+                .build();
 
-//     public SecurityController(AuthenticationManager authenticationManager, JwtEncoder jwtEncoder) {
-//         this.authenticationManager = authenticationManager;
-//         this.jwtEncoder = jwtEncoder;
-//     }
-//     @GetMapping("/profile")
-//     public Authentication authentication(Authentication auth) {
-//         return auth;
-//     }
-
-//     @PostMapping("/login")
-//     public Map<String, String> login(String username, String password) {
-//         Authentication auth = authenticationManager.authenticate(
-//                 new UsernamePasswordAuthenticationToken(username, password)
-//         );
-//         Instant instant = Instant.now();
-//         String scope = auth.getAuthorities().stream()
-//                 .map(grantedAuthority -> grantedAuthority.getAuthority())
-//                 .collect(Collectors.joining(" "));
-//         JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
-//                 .subject(username)
-//                 .issuedAt(instant)
-//                 .expiresAt(instant.plus(15, ChronoUnit.MINUTES))
-//                 .claim("scope", scope)
-//                 .build();
-
-//         JwtEncoderParameters params = JwtEncoderParameters.from(
-//         JwsHeader.with(MacAlgorithm.HS256).build(),
-//         jwtClaimsSet);
-//         String jwt = jwtEncoder.encode(params).getTokenValue();
-//         return Map.of("access_token", jwt);
-//     }
-// }
+        JwtEncoderParameters params = JwtEncoderParameters.from(
+        JwsHeader.with(MacAlgorithm.HS256).build(),
+        jwtClaimsSet);
+        String jwt = jwtEncoder.encode(params).getTokenValue();
+        return Map.of("access_token", jwt);
+    }
+}

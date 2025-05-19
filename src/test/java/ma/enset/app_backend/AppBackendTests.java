083de7553482f6c9729a950a6d36d9a34bc2a333
Abdex <abdex4social@gmail.com>
2025-05-19 08:54:13 +0100

    first commit

---
+++ src/test/java/ma/enset/app_backend/AppBackendTests.java
@@ -0,0 +1,13 @@
+package ma.enset.app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBackendTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
